04ed51e1a9f1b6ddac32214760b05513afe87ab6
yggene <yggene@gmail.com>
2021-07-30 21:04:06 +0300

    tested with arrays

--- 01_Swift_Basics/6l_EvgenyAlekseev/6l_EvgenyAlekseev/main.swift
+++ 01_Swift_Basics/6l_EvgenyAlekseev/6l_EvgenyAlekseev/main.swift
@@ -5,10 +5,11 @@
 //  Created by Evgeny Alekseev on 29.07.2021.
 //
 
+// testing basic functions on arrays of Int
 var myQ = Queue<Int>()
 myQ.enqueue(0, 1, 2)
 
-print("*** Basic queue methods check: ***")
+print("*** Checking basic methods: ***")
 print("My Queue: \(myQ)")
 print("My Queue count: \(myQ.count)")
 print("The first element of my queue:", myQ.peek() ?? "It's empty!")
@@ -22,18 +23,20 @@ print("Index -1:", myQ[-1] ?? "It's empty!")
 print("Index 2:", myQ[2] ?? "It's empty!")
 print("Index 1:", myQ[1] ?? "It's empty!")
 
-print("\n*** Checking high-order functions' implementation: ***")
-var myNewQ = Queue<Int>()
-myNewQ.enqueue(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+// testing high-order functions on arrays of Arrays
+var myNewQ = Queue<Array<Int>>()
+myNewQ.enqueue([1, 2], [3, 4], [5, 6], [7, 8], [9, 10])
+
+print("\n*** Checking high-order functions: ***")
 print("My new queue:", myNewQ)
 
-var myNewQFiltered = myNewQ.filter({ $0 % 2 != 0 })
+var myNewQFiltered = myNewQ.filter({ $0.contains(5) || $0.contains(1) })
 print("My new queue filtered:", myNewQFiltered)
 
-var myNewQMapped = myNewQ.map({ $0 * $0 })
+var myNewQMapped = myNewQ.map({ $0 + [($0.last ?? 0) + 1] })
 print("My new queue mapped:", myNewQMapped)
 
-var myNewQReduced = myNewQ.reduce(1, { $0 * $1 })
+var myNewQReduced = myNewQ.reduce([], { $0 + $1 })
 print("My new queue reduced result:", myNewQReduced)
 
-myNewQ.forEach({ print($0 * 3) })
+myNewQ.forEach({ print($0) })
