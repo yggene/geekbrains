428a280cddcf3bcc5469a2ab38bd737ee22585c0
yggene <yggene@gmail.com>
2021-08-22 23:35:25 +0300

    vkApp l2 added

---
+++ 02_UI_Design/fakeVKapp/fakeVKapp/Controllers/FriendsPhotosCollectionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FriendsCollectionViewController.swift
+//  fakeVKapp
+//
+//  Created by Evgeny Alekseev on 22.08.2021.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class FriendsPhotosCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+
+}
---
+++ 02_UI_Design/fakeVKapp/fakeVKapp/Controllers/FriendsTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FriendsTableViewController.swift
+//  fakeVKapp
+//
+//  Created by Evgeny Alekseev on 22.08.2021.
+//
+
+import UIKit
+
+class FriendsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+}
