beed6f54ec05574a0f13c146c401d463fd45b061
yggene <yggene@gmail.com>
2022-01-19 02:59:30 +0300

    switched appstartmanager to appBuild

--- 03_Architecture_and_Design_patterns/lesson7_architectures_demo_starter/iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
+++ 03_Architecture_and_Design_patterns/lesson7_architectures_demo_starter/iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
@@ -17,7 +17,7 @@ final class AppStartManager {
     }
     
     func start() {
-        let rootVC = SearchModuleBuilder.songBuild()
+        let rootVC = SearchModuleBuilder.appBuild()
         rootVC.navigationItem.title = "Search via iTunes"
         
         let navVC = self.configuredNavigationController
